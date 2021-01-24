691063412917c10639b4d2da74d6fe33c7045006
savannacampbell <74517038+savannacampbell@users.noreply.github.com>
2021-01-24 00:22:16 -0800

    added three arrays and values

---
+++ porfolioProjects/javascriptGame/altGameCode.js
@@ -0,0 +1,12 @@
+// use three pieces of data
+// ex: str + str + str
+
+const youOrMe = ['You are ', 'I am '];
+
+const midWord = ['loved', 'successful', 'okay', 'happy', 'wanted', 'cared for', 'funny'];
+
+const advice = [', so start paying attention to the people who matter.', 
+'; no small thing can bring you down',
+'; sit down and take a breather if you have to',
+'; they can wait, you come first']
+
