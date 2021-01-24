c21eb38622d434c1369d076743f26dfa83d1ff2f
savannacampbell <74517038+savannacampbell@users.noreply.github.com>
2021-01-24 00:25:51 -0800

    added sentence generator

--- porfolioProjects/javascriptGame/altGameCode.js
+++ porfolioProjects/javascriptGame/altGameCode.js
@@ -10,3 +10,11 @@ const advice = [', so start paying attention to the people who matter.',
 '; sit down and take a breather if you have to',
 '; they can wait, you come first']
 
+function sentenceGen () {
+    let choice1 = Math.floor(Math.random() * youOrMe.length);
+    let choice2 = Math.floor(Math.random() * midWord.length);
+    let choice3 = Math.floor(Math.random() * advice.length);
+    return youOrMe[choice1] + midWord[choice2] + advice[choice3];
+}
+
+console.log(sentenceGen());
